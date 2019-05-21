d5a0df23f049a952081d74116951d6aa8e3324aa
José Belo Delfino <jbelodelf@gmail.com>
2019-05-21 07:27:31 -0300

    Commit

---
+++ ProjetoDeTeste/WebApiCliente/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WebApiCliente.DTOS;
+using WebApiCliente.Models;
+
+namespace WebApiCliente.App_Start
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<ClienteEntity, ClienteDTO>().ReverseMap();
+            CreateMap<ChamadoEntity, ChamadoDTO>().ReverseMap();
+        }
+    }
+}
